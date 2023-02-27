a34f630f9d1423741910f2e176dcf892ba036a54
Manav Jawrani <manavjawrani7@gmail.com>
2023-02-28 01:23:37 +0530

    Update

--- sw.js
+++ sw.js
@@ -1,55 +1,81 @@
-const cacheName = 'Ecommerce';
+const cacheName = "Ecommerce";
 const staticAssets = [
-    './',
-    './index.html',
-    './about.html',
-    './drip.html',
-    './electronics.html',
-    './furniture.html',
-    './general.html',
-    './index.html',
-    './laptops.html',
-    './phones.html',
-    './sneakers.html',
-    './manifest.json',
-    './style.css',
+  "./",
+  "./index.html",
+  "./about.html",
+  "./drip.html",
+  "./electronics.html",
+  "./furniture.html",
+  "./general.html",
+  "./index.html",
+  "./laptops.html",
+  "./phones.html",
+  "./sneakers.html",
+  "./manifest.json",
+  "./style.css",
 ];
 
-self.addEventListener('install',async e =>{
-    const cache = await caches.open(cacheName);
-    await cache.addAll(staticAssets);
-    return self.skipWaiting();
-});
- 
-self.addEventListener('activate',e =>{
-    self.clients.claim();
+// Cache static assets on install
+self.addEventListener("install", async () => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
 });
 
-self.addEventListener('fetch', async e => {
-    const req = e.request;
-    const url = new URL(req.url);
-  
-    if (url.origin === location.origin) {
-      e.respondWith(cacheFirst(req));
-    } else {
-      e.respondWith(networkAndCache(req));
-    }
-  });
-  
-  async function cacheFirst(req) {
-    const cache = await caches.open(cacheName);
-    const cached = await cache.match(req);
-    return cached || fetch(req);
+// Activate service worker and claim clients
+self.addEventListener("activate", () => {
+  self.clients.claim();
+});
+
+// Serve assets from cache first, then from network
+self.addEventListener("fetch", async (event) => {
+  const request = event.request;
+  const url = new URL(request.url);
+
+  if (url.origin === location.origin) {
+    event.respondWith(cacheFirst(request));
+  } else {
+    event.respondWith(networkAndCache(request));
   }
-  
-  async function networkAndCache(req) {
-    const cache = await caches.open(cacheName);
-    try {
-      const fresh = await fetch(req);
-      await cache.put(req, fresh.clone());
-      return fresh;
-    } catch (e) {
-      const cached = await cache.match(req);
-      return cached;
+});
+
+async function cacheFirst(request) {
+  const cache = await caches.open(cacheName);
+  const cached = await cache.match(request);
+  return cached || fetch(request);
+}
+
+async function networkAndCache(request) {
+  const cache = await caches.open(cacheName);
+  try {
+    const response = await fetch(request);
+    await cache.put(request, response.clone());
+    return response;
+  } catch (error) {
+    const cached = await cache.match(request);
+    return cached;
+  }
+}
+
+// Handle push notifications
+self.addEventListener("push", function (event) {
+  if (event && event.data) {
+    const data = event.data.json();
+    if (data.method === "pushMessage") {
+      console.log("Push notification sent");
+      event.waitUntil(
+        self.registration.showNotification("Omkar Sweets Corner", {
+          body: data.message,
+          icon: "path/to/icon.png",
+        })
+      );
     }
   }
+});
+
+// Handle background sync
+self.addEventListener("sync", (event) => {
+  if (event.tag === "event1") {
+    console.log("Sync successful!");
+  }
+});
